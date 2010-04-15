09fdfb803600c20155036f70f83c072d9fe27393
Nick Stenning <nick@whiteink.com>
2010-04-15 00:52:14 -0400

    Explicitly set gif = 0 for movierot

--- plot.sh
+++ plot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ -n "${1}" && -n "${2}" ]]; then 
-  exec gnuplot -e "d='`pwd`'" -e "cd '${1}'" -e "load d.'/gnuplot/${2}'"
+  exec gnuplot -e "d='`pwd`'" -e "gif = 0" -e "cd '${1}'" -e "load d.'/gnuplot/${2}'"
 else
   echo "Usage: ./plot.sh <datadir> <plottype>!"
   exit 1
