c3ced4a8551f82590caa9acd354bf92117bfd829
Nick Stenning <nick@whiteink.com>
2010-04-24 10:47:08 +0100

    Update join call -- don't cut out headers

--- gen-positions-only.sh
+++ gen-positions-only.sh
@@ -2,7 +2,9 @@
 
 if [[ -n "${1}" ]]; then 
   pushd "${1}"
-  join <(join jupiter trojans | grep -v '^#' | cut -d' ' -f 1,2,3,9,10) greeks | cut -d' ' -f1-7 > positions-only 2> /dev/null
+  join <(join jupiter trojans | cut -d' ' -f 1,2,3,9,10) greeks | cut -d' ' -f1-7 > positions-only-tmp
+  join system positions-only-tmp > positions-only
+  rm positions-only-tmp
   popd
 else
   echo "Usage: ./gen-positions-only.sh <datadir>!"
