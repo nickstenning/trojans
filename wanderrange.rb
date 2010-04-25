64524ee58b0fe90f90b7787cc18616a77b0f07bd
Nick Stenning <nick@whiteink.com>
2010-04-25 18:47:31 +0100

    Calculate mean, not max, wander range

--- wanderrange.rb
+++ wanderrange.rb
@@ -35,9 +35,7 @@ t_m_y /= lines.count
 g_m_x /= lines.count
 g_m_y /= lines.count
 
-inf = 1.0/0
-
-t_max, g_max = lines.inject([-inf, -inf]) do |(tmx, gmx), x|
+t_deltas, g_deltas = lines.inject([[], []]) do |(tmx, gmx), x|
   bar_x, bar_y = rot_coords(x[BRY], x[BRY+1], x[ANG])
   t_x, t_y = rot_coords(x[TRO], x[TRO+1], x[ANG])
   g_x, g_y = rot_coords(x[GRE], x[GRE+1], x[ANG])
@@ -47,14 +45,14 @@ t_max, g_max = lines.inject([-inf, -inf]) do |(tmx, gmx), x|
   g_x -= bar_x
   g_y -= bar_y
   
-  # Calculate distance from average position
-  dt = sqrt((t_m_x - t_x)**2 + (t_m_y - t_y)**2)
-  dg = sqrt((g_m_x - g_x)**2 + (g_m_y - g_y)**2)
+  tmx << sqrt((t_m_x - t_x)**2 + (t_m_y - t_y)**2)
+  gmx << sqrt((g_m_x - g_x)**2 + (g_m_y - g_y)**2)
   
-  tmx = dt if dt > tmx
-  gmx = dg if dg > gmx
-  
+
   [tmx, gmx]
 end
 
-p (t_max + g_max) / 2
+t_mean = t_deltas.inject(0) { |acc, x| acc + x } / t_deltas.length
+g_mean = g_deltas.inject(0) { |acc, x| acc + x } / g_deltas.length
+
+p (t_mean + g_mean) / 2
